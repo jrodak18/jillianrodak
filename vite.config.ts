0046059ee8b1e2f996b1085fa71699226411c637
Dan Flatt <108902970+Flattorama@users.noreply.github.com>
2025-11-26 15:29:26 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/jillianrodak/', // <--- This is the key change for GitHub Pages
+      base: '/jillianrodak/', // <--- THIS LINE IS CRITICAL
       server: {
         port: 3000,
         host: '0.0.0.0',
